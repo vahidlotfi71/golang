e0b7ed42175bc20439819e3b9b6337768fd305cc
vahid lotfi <v.lotfi71@gmail.com>
2024-10-17 12:46:46 +0330

    p8 s1_3

--- practice_2.go
+++ practice_2.go
@@ -12,37 +12,71 @@ func main() {
 
 	//================================================================
 
-	order_1, tax_1 := calculateRoomPrice("silge", 2, 2)
-	order_2 , tax_2 := calculateRoomPrice("suite",3 , 4)
+	// order_1, tax_1 := calculateRoomPrice("silge", 2, 2)
+	// order_2 , tax_2 := calculateRoomPrice("suite",3 , 4)
 
-	fmt.Printf("Oder_1: %v , tax: %.2f\n", order_1, tax_1)
-	fmt.Printf("Oder_2: %v ,tax: %.2f\n", order_2, tax_2)
+	// fmt.Printf("Oder_1: %v , tax: %.2f\n", order_1, tax_1)
+	// fmt.Printf("Oder_2: %v ,tax: %.2f\n", order_2, tax_2)
+
+	//====================================================================
+
+	oder1 , finalPrice , tax := calculate("single" , 1 , 1)
+
+	fmt.Printf("price : %v ,tax: %.2f , totalPrice : %f\n", finalPrice, tax , oder1)
 
 }
 
-func calculateRoomPrice(roomType string, night int, personNumber int) (int, float64) {
-	var price int
-	var tax float64
+
+
+func calculate(roomType string , night int , personNumber int ) (totalPrice float64, price int , tax float64){ // همین جا خورجی هامون را مشخص می کنم
 
 	switch roomType {
-	case "silge":
-			price = night * personNumber * 100
-
-	case "dubble":
-			price = night * personNumber * 200
+	case "single":
+		price = night * personNumber * 100
 		
+	case "duble":
+		price = night * personNumber * 200
+	
 	case "suite":
-			price = night * personNumber * 400
+		price = night * personNumber * 400
 	default :
-			println("Unknown type for roomtype")
-			break
+		println("unknown roomType value")
+		break
 	}
+	price = price
+	tax = float64(price) * 0.09
+	totalPrice = float64(price) + float64(tax)
+
+	return  // دیگر دراینجاخروجی هامون رانمی اوریم
+
+}
+
+
+
+
+// func calculateRoomPrice(roomType string, night int, personNumber int) (int, float64) {
+// 	var price int
+// 	var tax float64
+
+// 	switch roomType {
+// 	case "silge":
+// 			price = night * personNumber * 100
+
+// 	case "dubble":
+// 			price = night * personNumber * 200
+		
+// 	case "suite":
+// 			price = night * personNumber * 400
+// 	default :
+// 			println("Unknown type for roomtype")
+// 			break
+// 	}
 
 	
-	tax = float64(price) * 0.09
-	price = price + (int(tax))
-	return price, tax
-}	
+// 	tax = float64(price) * 0.09
+// 	price = price + (int(tax))
+// 	return price, tax
+// }	
 
 // func print_1(){
 // 	println("hello vahid")
